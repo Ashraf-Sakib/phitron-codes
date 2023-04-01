ded216829c8d400e9f9c3dae06e87909952c0001
hasanAshraful <ashrafulhasan601@gmail.com>
2023-04-02 01:39:51 +0600

    hello.c

--- VSetup/hello.c
+++ VSetup/hello.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    printf("hello world\n I am from Bangladesh\nMy name is Ashraful Hasan");
+    printf("hello world\n I am from Bangladesh\nMy name is Ashraful Hasan\nI am an undergradute student");
     return 0;
 }
